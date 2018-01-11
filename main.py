efad000727af49fabb2f4e76b743147f3a924e78
avmoran <avmoran@wustl.edu>
2018-01-11 11:11:19 -0600

    Refactored to implement MVC

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLAlchemy
-import cgi
-
-app = Flask(__name__)
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:flicklist@localhost:8889/flicklist'
-app.config['SQLALCHEMY_ECHO'] = True
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    movies = db.relationship('Movie', backref='owner')
-    
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.email
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    watched = db.Column(db.Boolean)
-    rating = db.Column(db.String(20))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, name, owner):
-        self.name = name
-        self.owner = owner
-        self.watched = False
-
-    def __repr__(self):
-        return '<Movie %r>' % self.name
-
+from flask import request, redirect, render_template, session, flash
+from models import User, Movie
+from app import app, db
 # a list of movie names that nobody should have to watch
 terrible_movies = [
     "Gigli",
